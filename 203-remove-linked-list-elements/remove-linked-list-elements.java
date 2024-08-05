3e95d5a7502c5540b2e248722d0861296328bd91
Pratik Sinha <91456061+pks2906@users.noreply.github.com>
2024-08-05 10:25:59 +0530

    Time: 1 ms (91.93%) | Memory: 45.4 MB (59.59%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp=head;
+        ListNode h=null;
+        if(head!=null&&head.val==val)
+        {
+            h=head;
+            while(h!=null)
+            {
+                if(head.val==val)
+                {
+                    head=head.next;
+                }
+                else
+                break;
+                h=h.next;
+            }
+        }
+        temp=head;
+        while(temp!=null)
+        {
+            if(temp.next==null)
+            {
+                break;
+            }
+            else
+            {
+                if(temp.next.val==val)
+                {
+                    if(temp.next.next==null)
+                    {
+                        temp.next=null;
+                        break;
+                    }
+                    else
+                    {
+                        // temp.next=temp.next.next;
+                        ListNode y=temp.next;
+                        while(y!=null)
+                        {
+                            if(y.val==val)
+                            {
+                                temp.next=y.next;
+                            }
+                            else
+                            break;
+                            y=y.next;
+                        }
+                    }
+                }
+            }
+            temp=temp.next;
+        }
+
+        return head;
+    }
+}
