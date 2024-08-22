ba65bb4fbcb9c5fba28cf7e7551a2aa7ecc1281e
Pratik Sinha <91456061+pks2906@users.noreply.github.com>
2024-08-22 20:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (8.34%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,8 +1,19 @@
 class Solution {
- public:
-  int findComplement(long num) {
-    for (long i = 1; i <= num; i <<= 1)
-      num ^= i;
-    return num;
-  }
+public:
+    int findComplement(int num) {
+        string s;
+        while(num != 0 ){
+            if(num%2==0)  s = '1'+s;
+            else  s = '0'+s;
+
+            num = num/2;
+        }
+        int ans=0;
+        int p=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='1') ans += pow(2,p);
+            p++;
+        }
+        return ans;
+    }
 };
