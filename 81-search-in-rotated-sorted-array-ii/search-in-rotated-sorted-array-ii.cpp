b533eaa4595b161a37a2f9104a59969a33c2f603
Pratik Sinha <91456061+pks2906@users.noreply.github.com>
2024-10-14 00:22:39 +0530

    Time: 3 ms (79.99%) | Memory: 16.6 MB (89.50%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,29 +1,38 @@
 class Solution {
- public:
-  bool search(vector<int>& nums, int target) {
-    int l = 0;
-    int r = nums.size() - 1;
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
 
-    while (l <= r) {
-      const int m = (l + r) / 2;
-      if (nums[m] == target)
-        return true;
-      if (nums[l] == nums[m] && nums[m] == nums[r]) {
-        ++l;
-        --r;
-      } else if (nums[l] <= nums[m]) {  // nums[l..m] are sorted
-        if (nums[l] <= target && target < nums[m])
-          r = m - 1;
-        else
-          l = m + 1;
-      } else {  // nums[m..n - 1] are sorted
-        if (nums[m] < target && target <= nums[r])
-          l = m + 1;
-        else
-          r = m - 1;
-      }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;  
+            if (nums[mid] == target) return true;
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
+            }
+
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } 
+           
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
     }
-
-    return false;
-  }
 };
