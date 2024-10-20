0d8026d471749011b59fefaea2a2d9532453edff
Pratik Sinha <91456061+pks2906@users.noreply.github.com>
2024-10-20 20:34:41 +0530

    Time: 31 ms (99.60%) | Memory: 69.2 MB (91.39%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,37 +1,43 @@
+bool possible(vector<int> &arr, int day, int r, int b){
+	int cnt = 0;
+	int noOfB = 0;
+
+	for(int i = 0; i<arr.size(); i++){
+		if(arr[i] <= day){
+			cnt++;
+		}
+		else{
+			noOfB += (cnt/b);
+			cnt = 0;
+		}
+	}
+
+	noOfB += (cnt/b);
+	return noOfB >= r;
+}
+
+
 class Solution {
- public:
-  int minDays(vector<int>& bloomDay, int m, int k) {
-    if (bloomDay.size() < static_cast<long>(m) * k)
-      return -1;
+public:
+    int minDays(vector<int>& bloomDay, int k, int m) {
+    long long val = k * 1LL * m * 1LL;
+	if(val > bloomDay.size()) return -1;
+	int mini = INT_MAX, maxi = INT_MIN;
+	for(int i = 0; i<bloomDay.size(); i++){
+		mini = min(mini, bloomDay[i]);
+		maxi = max(maxi, bloomDay[i]);
+	}
 
-    int l = ranges::min(bloomDay);
-    int r = ranges::max(bloomDay);
+	int low = mini, high = maxi;
+	while(low <= high){
+		int mid = (low + high) / 2;
+		if(possible(bloomDay, mid, k, m)){
+			high = mid - 1;
+		}
+		else low = mid + 1;
+	}
 
-    while (l < r) {
-      const int mid = (l + r) / 2;
-      if (getBouquetCount(bloomDay, k, mid) >= m)
-        r = mid;
-      else
-        l = mid + 1;
-    }
-
-    return l;
-  }
-
- private:
-  
-  int getBouquetCount(const vector<int>& bloomDay, int k, int waitingDays) {
-    int bouquetCount = 0;
-    int requiredFlowers = k;
-    for (const int day : bloomDay)
-      if (day > waitingDays) {
+	return low;
         
-        requiredFlowers = k;
-      } else if (--requiredFlowers == 0) {
-       
-        ++bouquetCount;
-        requiredFlowers = k;
-      }
-    return bouquetCount;
-  }
+    }
 };
