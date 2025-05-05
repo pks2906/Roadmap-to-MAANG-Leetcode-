67c23a2fcae77ab01b406a56a89040157f2c462c
Pratik Sinha <91456061+pks2906@users.noreply.github.com>
2025-05-05 11:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (49.77%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,22 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int findMaxPathSum(TreeNode* root, int& maxi) {
-        if (root == nullptr) {
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        maxPathDown(root, maxi);
+        return maxi;   
+    }
+
+private:
+    int maxPathDown(TreeNode* node, int& maxi) {
+        if (node == nullptr) {
             return 0;
         }
 
-        int leftMaxPath = std::max(0, findMaxPathSum(root->left, maxi));
-        int rightMaxPath = std::max(0, findMaxPathSum(root->right, maxi));
+        int left = max(0, maxPathDown(node->left, maxi));
+        int right = max(0, maxPathDown(node->right, maxi));
 
-        // Use root->val instead of root->data
-        maxi = std::max(maxi, leftMaxPath + rightMaxPath + root->val);
+        maxi = max(maxi, left + right + node->val);  
 
-        return std::max(leftMaxPath, rightMaxPath) + root->val;
-    }
-
-    int maxPathSum(TreeNode* root) {
-        int maxi = INT_MIN;
-        findMaxPathSum(root, maxi);
-        return maxi;
+        return max(left, right) + node->val;  
     }
 };
