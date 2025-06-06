3d8264b3625c7b4e4967813c15577ef18d73e996
Pratik Sinha <91456061+pks2906@users.noreply.github.com>
2025-06-06 13:31:20 +0530

    Time: 4 ms (58.77%) | Memory: 27.5 MB (55.90%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int, int> inMap;
+        for (int i = 0; i < inorder.size(); i++) {
+            inMap[inorder[i]] = i;
+        }
+
+        TreeNode* root = buildTree(preorder, 0, preorder.size() - 1, inorder, 0,
+                                   inorder.size() - 1, inMap);
+
+        return root;
+    }
+
+private:
+    // Recursive helper function to build the tree
+    TreeNode* buildTree(vector<int>& preorder, int preStart, int preEnd,
+                        vector<int>& inorder, int inStart, int inEnd,
+                        map<int, int>& inMap) {
+
+        if (preStart > preEnd || inStart > inEnd) {
+            return NULL;
+        }
+
+        TreeNode* root = new TreeNode(preorder[preStart]);
+
+        int inRoot = inMap[root->val];
+
+        int numsLeft = inRoot - inStart;
+
+        // Recursively build the left subtree
+        root->left = buildTree(preorder, preStart + 1, preStart + numsLeft,
+                               inorder, inStart, inRoot - 1, inMap);
+
+        // Recursively build the right subtree
+        root->right = buildTree(preorder, preStart + numsLeft + 1, preEnd,
+                                inorder, inRoot + 1, inEnd, inMap);
+
+        return root;
+    }
+};
