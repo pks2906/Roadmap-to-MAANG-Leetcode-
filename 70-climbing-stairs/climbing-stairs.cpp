46633dab319b80830586176131795307fe820f87
Pratik Sinha <91456061+pks2906@users.noreply.github.com>
2025-06-16 22:31:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (98.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,17 @@
 class Solution {
- public:
-  int climbStairs(int n) {
-    // dp[i] := # of distinct ways to climb to i-th stair
-    vector<int> dp(n + 1);
-    dp[0] = 1;
-    dp[1] = 1;
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) return 1;
 
-    for (int i = 2; i <= n; ++i)
-      dp[i] = dp[i - 1] + dp[i - 2];
+        int prev2 = 1;
+        int prev = 1;
 
-    return dp[n];
-  }
+        for (int i = 2; i <= n; i++) {
+            int cur_i = prev2 + prev;
+            prev2 = prev;
+            prev = cur_i;
+        }
+
+        return prev;  
+    }
 };
