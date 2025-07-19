f71fcc5e1fdc495557fc2524f4418a2665b8da32
Pratik Sinha <91456061+pks2906@users.noreply.github.com>
2025-07-19 13:40:30 +0530

    Time: 44 ms (35.42%) | Memory: 49.5 MB (35.32%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     char processStr(string s, long long k) {
         int n = s.size();
-        vector<long long> len(n + 1); // len[i] = length of result after processing s[0...i-1]
+        vector<long long> len(n + 1); 
         len[0] = 0;
 
         for (int i = 0; i < n; ++i) {
@@ -16,14 +16,12 @@ public:
                 if (len[i] >= 5e14) len[i+1] = 1e15;
                 else len[i+1] = len[i] * 2;
             } else if (c == '%') {
-                len[i+1] = len[i]; // length stays same
+                len[i+1] = len[i]; 
             }
         }
 
         if (k >= len[n]) return '.';
 
-        // Step 2: Go back in reverse to deduce the k-th character
-        // This is like recursively unraveling the operations
         for (int i = n - 1; i >= 0; --i) {
             char c = s[i];
             if (islower(c)) {
@@ -32,23 +30,18 @@ public:
                         return c;
                     }
                 }
-                // else, current length didn't change, skip
             } else if (c == '*') {
                 if (len[i] + 1 == len[i+1]) {
-                    // character was removed, so we can't retrieve it
-                    // nothing to do
+                    
                 } else {
-                    // coming from reduced length, no effect
                 }
             } else if (c == '#') {
                 if (k < len[i]) {
                 } else {
-                    // Second half of duplication came from first half
-                    // map k to k - len[i]
                     k -= len[i];
                 }
             } else if (c == '%') {
-                k = len[i] - 1 - k; // reverse the index
+                k = len[i] - 1 - k; 
             }
         }
 
